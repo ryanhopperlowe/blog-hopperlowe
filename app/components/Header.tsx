b2f9aba77d0ac62f33a645d4d4965c5a294db831
Ryan Hopper-Lowe <rhopperlowe@gmail.com>
2024-12-15 22:50:30 -0600

    add social links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,8 @@ import {
   NavbarContent,
   NavbarItem,
 } from "@nextui-org/react";
-import { useMatches } from "@remix-run/react";
+import { useMatches, Link as RemixLink } from "@remix-run/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export function Header({ isAuthed }: { isAuthed: boolean }) {
   const matches = useMatches();
@@ -25,7 +26,7 @@ export function Header({ isAuthed }: { isAuthed: boolean }) {
         </Link>
       </NavbarBrand>
 
-      <NavbarContent justify="center">
+      <NavbarContent justify="end">
         <NavbarItem isActive={isBlogActive}>
           <Link
             href="/blog"
@@ -35,19 +36,34 @@ export function Header({ isAuthed }: { isAuthed: boolean }) {
             Blog
           </Link>
         </NavbarItem>
-      </NavbarContent>
+        <NavbarItem>
+          <RemixLink
+            to="https://github.com/ryanhopperlowe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={22} />
+          </RemixLink>
+        </NavbarItem>
 
-      {isAuthed && (
-        <NavbarContent justify="end">
-          <>
-            <NavbarItem>
-              <Button variant="solid" color="primary" as={Link} href="/logout">
-                Logout
-              </Button>
-            </NavbarItem>
-          </>
-        </NavbarContent>
-      )}
+        <NavbarItem>
+          <RemixLink
+            to="https://www.linkedin.com/in/ryan-hopper-lowe-33ba95135/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin size={22} />
+          </RemixLink>
+        </NavbarItem>
+
+        {isAuthed && (
+          <NavbarItem>
+            <Button variant="solid" color="primary" as={Link} href="/logout">
+              Logout
+            </Button>
+          </NavbarItem>
+        )}
+      </NavbarContent>
     </Navbar>
   );
 }
